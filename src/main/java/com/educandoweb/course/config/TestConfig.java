f203c14c99a0fecbb236d160bc53236f1a23f533
luan.nascimento <luan.nascimento@segsat.com>
2024-09-25 14:07:53 -0300

    Category

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.enums.OrderStatus;
+import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
@@ -24,8 +26,15 @@ public class TestConfig implements CommandLineRunner{
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+        
+        Category c1 = new Category(null, "Eletronics");
+        Category c2 = new Category(null, "Books");
+        Category c3 = new Category(null, "Computers");
         
         User u1 = new User(null, "L", "l@gmail.com", "123124123", "12345");
         User u2 = new User(null, "C", "c@gmail.com", "819231212", "12345"); 
@@ -33,6 +42,8 @@ public class TestConfig implements CommandLineRunner{
         Order o1 = new Order(null, Instant.parse("2024-09-21T09:15:02Z"), OrderStatus.PAID , u1);
         Order o2 = new Order(null, Instant.parse("2024-09-25T09:15:02Z"), OrderStatus.WAITING_PAYMENT, u1);
         Order o3 = new Order(null, Instant.parse("2024-09-23T09:15:02Z"), OrderStatus.WAITING_PAYMENT, u2);
+
+        categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
 
         userRepository.saveAll(Arrays.asList(u1, u2));
 
